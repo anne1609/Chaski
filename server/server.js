85f120c562948de1232debb5702dd537bd2d544c
Anaelise Huaniri Romero <aanesita@gmail.com>
2025-05-01 23:33:37 -0400

    Initial structure: React client and Express server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+};
+app.use(cors(corsOptions));
+app.get("/api", (req, res) => {
+    res.json({ fruits: ["apple", "orange", "pineapple"] });
+  });
+  
+  app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+  });
