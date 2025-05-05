16184e6c3a3f3a7c636aedb585c9c71100b3a477
Anaelise Huaniri Romero <aanesita@gmail.com>
2025-05-05 09:55:33 -0400

    ui: add 'Enviar Correo' button to secretaria view

--- client/src/components/secretary/Secretary.jsx
+++ client/src/components/secretary/Secretary.jsx
@@ -1,6 +1,28 @@
 
+import React, { useState } from 'react';
+import EmailForm from '../shared/EmailForm';
+import { Button, Box } from '@mui/material';
+
 function Secretary() {
-  return <h1>Hola soy la secretaria</h1>;
+  const [showForm, setShowForm] = useState(false);
+
+  const handleToggleForm = () => {
+    setShowForm((prev) => !prev);
+  };
+
+  return (
+    <Box sx={{ textAlign: 'center', mt: 5 }}>
+      <h1>Hola, soy la secretaria</h1>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleToggleForm}
+      >
+        {showForm ? 'Ocultar Formulario' : 'Enviar Correo'}
+      </Button>
+      {showForm && <EmailForm role="secretary" />}
+    </Box>
+  );
 }
 
 export default Secretary;
