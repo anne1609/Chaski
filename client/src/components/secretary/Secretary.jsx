56bc47754fa02da9d05e8fabd3a32ae291ec75db
4drianC4 <camararequeadrianrafael@gmail.com>
2025-05-08 14:57:23 -0400

    feat: creation of communications list

--- client/src/components/secretary/Secretary.jsx
+++ client/src/components/secretary/Secretary.jsx
@@ -1,14 +1,20 @@
 
 import React, { useState } from 'react';
 import EmailForm from '../shared/emailForm';
+import CommunicationsStatus from '../shared/CommunicationsStatus';
 import { Button, Box } from '@mui/material';
 
 function Secretary() {
   const [showForm, setShowForm] = useState(false);
+  const [showStatus, setShowStatus] = useState(false);
 
   const handleToggleForm = () => {
     setShowForm((prev) => !prev);
   };
+
+  const handleToggleStatus = () => {
+    setShowStatus((prev) => !prev);
+  }
 
   return (
     <Box sx={{ textAlign: 'center', mt: 5 }}>
@@ -17,10 +23,20 @@ function Secretary() {
         variant="contained"
         color="primary"
         onClick={handleToggleForm}
+        sx={{ mt: 2, ml: 2 }}
       >
         {showForm ? 'Ocultar Formulario' : 'Enviar Correo'}
       </Button>
       {showForm && <EmailForm role="secretary" />}
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={handleToggleStatus}
+        sx={{ mt: 2, ml: 2 }}
+      >
+        {showStatus ? 'Ocultar Estado' : 'Mostrar Estado'}
+      </Button>
+      {showStatus && <CommunicationsStatus />}
     </Box>
   );
 }
