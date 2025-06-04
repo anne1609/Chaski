5e5bdb6f5c27b08093b45fb22fcb03a211e17531
Anaelise Huaniri Romero <aanesita@gmail.com>
2025-06-04 17:04:07 -0400

    Update: statuses now reflect 'rejected' and 'confirmed' states correctly

--- client/src/components/shared/RejectionPage.jsx
+++ client/src/components/shared/RejectionPage.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
-
+import React,{ useEffect }  from 'react';
+import { useLocation } from 'react-router-dom';
+  
 const RejectionPage = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const communicationId = queryParams.get('communication_id');
+  const teacherId = queryParams.get('teacher_id');
+  
+  useEffect(() => {
+    if (communicationId) {
+      fetch(`http://localhost:8080/api/communication/${communicationId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          attendance_status: 'Rechazado',
+        }),
+      });
+    }
+  }, [communicationId]);
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>😔 ¡Qué pena!</h1>
--- server/controllers/communicationController.js
+++ server/controllers/communicationController.js
@@ -88,7 +88,7 @@ module.exports = {
     },
     async updateCommunication(req, res) {
         const { id } = req.params;
-        const { category_id, secretary_id, teacher_id, subject, body, status, priority } = req.body;
+        const { category_id, secretary_id, teacher_id, subject, body, status, priority, meeting_datetime,  attendance_status,attachment } = req.body;
         try {
             const communication = await Communications.findByPk(id);
             if (!communication) {
@@ -102,6 +102,9 @@ module.exports = {
                 body,
                 status,
                 priority,
+                meeting_datetime,
+                attendance_status,
+                attachment 
             });
             res.status(200).json(communication);
         } catch (error) {
